d9a6fbde7465405919e1d97d9a2e5e05659399b1
89E <89e@JU.local>
2024-09-23 07:16:33 +0800

    update main file

--- chatpdf.py
+++ chatpdf.py
@@ -119,7 +119,7 @@ def main():
     st.markdown(
         f"""
         <div style="display: flex; justify-content: center;">
-            <img src="data:image/png;base64,{st.image(logo_path, use_column_width=True)}" style="width: 12px; height: 12px;">
+            <img src="data:image/png;base64,{st.image(logo_path, use_column_width=False)}" style="width: 12px; height: 12px;">
         </div>
         """,
         unsafe_allow_html=True
